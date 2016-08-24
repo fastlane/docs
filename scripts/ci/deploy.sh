b17a508891743c86d71c0567748a75cd31796d7f
Felix Krause <krausefx@gmail.com>
2016-08-24 15:25:12 -0700

    Remove old code

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -8,12 +8,6 @@ echo "Started deploying to https://docs.fastlane.tools"
 
 mkdocs gh-deploy --clean --remote-branch "gh-pages" --remote-name "upstream" --verbose
 
-# To make sure pushing was successful and to see the error message if it wasn't,
-# we have to manually push like an animal
-git checkout gh-pages
-git status
-git push origin gh-pages
-
 echo "Deployed successfully, check out https://docs.fastlane.tools"
 
 exit 0
