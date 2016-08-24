468b0152a11c57119044fb001d4f9b337479a412
Felix Krause <krausefx@gmail.com>
2016-08-24 15:05:36 -0700

    Add some debug code

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -8,6 +8,12 @@ echo "Started deploying to https://docs.fastlane.tools"
 
 mkdocs gh-deploy
 
+# To make sure pushing was successful and to see the error message if it wasn't,
+# we have to manually push like an animal
+git checkout gh-pages
+git status
+git push origin gh-pages
+
 echo "Deployed successfully, check out https://docs.fastlane.tools"
 
 exit 0
