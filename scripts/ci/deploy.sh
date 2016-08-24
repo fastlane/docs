c5a55296b5d886f101e66a96d7453fcec4f1569c
Felix Krause <krausefx@gmail.com>
2016-08-24 13:59:11 -0700

    Simplfiy deploy script

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -4,11 +4,7 @@
 # Exit if any subcommand fails.
 set -e
 
-ORIGIN_URL=`git config --get remote.origin.url`
 echo "Started deploying to https://docs.fastlane.tools"
-
-git config user.name "$USER_NAME"
-git config user.email "$USER_EMAIL"
 
 mkdocs gh-deploy
 
