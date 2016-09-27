f0faccd436210d6c0002ee92c6e5fa8d95b1cad9
Felix Krause <KrauseFx@users.noreply.github.com>
2016-09-27 12:47:25 -0700

    Remove substring call

--- scripts/ci/generate_redirects.rb
+++ scripts/ci/generate_redirects.rb
@@ -9,7 +9,7 @@ docs_redirects.each do |from, to|
 
   full_path = File.join(from, "index.html")
   to = "/#{to}" unless to.start_with?("/")
-  content = "<script type='text/javascript'>window.location.href = '#{to}#' + window.location.hash.substring(1);</script>"
+  content = "<script type='text/javascript'>window.location.href = '#{to}#' + window.location.hash;</script>"
 
   File.write(full_path, content)
   puts "Successfully created file at path '#{full_path}' to point to '#{to}'"
