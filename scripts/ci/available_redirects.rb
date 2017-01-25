c9a14a37abe91869cc0ae52e43cb8a75192c9fab
Michael Furtak <michael.furtak@gmail.com>
2017-01-25 10:37:59 -0500

    Remove case-only redirects (#266)
    Case-only redirects are causing infinite redirect
    loops in production, so this is a change to try
    to fix that.

--- scripts/ci/available_redirects.rb
+++ scripts/ci/available_redirects.rb
@@ -1,19 +1,14 @@
-def docs_redirects 
+def docs_redirects
   {
     "codesigning/GettingStarted" => "codesigning/getting-started",
     "codesigning/XcodeProject/" => "/codesigning/xcode-project/",
-    "codesigning/Troubleshooting/" => "/codesigning/troubleshooting/",
     "codesigning/CommonIssues" => "codesigning/common-issues",
     "plugins/AvailablePlugins" => "plugins/available-plugins",
     "plugins/CreatePlugin" => "plugins/create-plugin",
     "plugins/PluginsTroubleshooting" => "plugins/plugins-troubleshooting",
     "actions/Actions" => "actions",
-    "FAQs" => "faqs",
-    "best-practices/Gitignore" => "best-practices/source-control",
     "best-practices/gitignore" => "best-practices/source-control",
     "best-practices/SourceControl" => "best-practices/source-control",
-    "best-practices/ContinuousIntegration" => "best-practices/continuous-integration",
-    "best-practices/Keys" => "best-practices/keys",
-    "Advanced" => "advanced"
+    "best-practices/ContinuousIntegration" => "best-practices/continuous-integration"
   }
 end
