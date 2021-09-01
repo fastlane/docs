b7bbab8ff841304c572809f3b1b68be06c5808b2
Lukasz Grabowski <luc.grabowski@gmail.com>
2021-09-02 00:52:27 +0200

    Adjust hooks to remove actions directory

--- hooks.py
+++ hooks.py
@@ -3,6 +3,7 @@ import shutil
 def copy_generated_files(*args, **kwargs):
     shutil.copy("docs/generated/available-plugins.md", "docs/plugins/available-plugins.md")
     shutil.copy("docs/generated/actions.md", "docs/actions.md")
+    shutil.rmtree("docs/actions")
     shutil.copytree("docs/generated/actions", "docs/actions")
 
 if __name__ == "__main__":
