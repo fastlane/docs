c81ff55c5203ece45d01d5af95428693f5b96236
Lukasz Grabowski <luc.grabowski@gmail.com>
2021-09-02 00:29:00 +0200

    Add mkdocs-simple-hooks plugin to run python script that copies generated files to proper places

---
+++ hooks.py
@@ -0,0 +1,9 @@
+import shutil
+
+def copy_generated_files(*args, **kwargs):
+    shutil.copy("docs/generated/available-plugins.md", "docs/plugins/available-plugins.md")
+    shutil.copy("docs/generated/actions.md", "docs/actions.md")
+    shutil.copytree("docs/generated/actions", "docs/actions")
+
+if __name__ == "__main__":
+    copy_generated_files()
